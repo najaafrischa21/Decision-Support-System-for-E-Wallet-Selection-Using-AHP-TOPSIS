776912333217e9d0b53357d0ff696aed75d81626
najaafrischa21 <161110726+najaafrischa21@users.noreply.github.com>
2026-07-22 20:29:33 +0700

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,5 @@
+<nav class="navbar navbar-expand-lg bg-white border-bottom px-4 style="position: sticky; top: 0; z-index: 1000;"">
+  <div class="container-fluid">
+    <a class="navbar-brand text-primary fw-bold" href="#">Pemilihan E-Wallet Terbaik Dengan Metode AHP-TOPSIS</a>
+  </div>
+</nav>
